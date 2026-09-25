28553e122c42abbaf8407cfd1a96c26887587351
Prem Kumar <24pa1a05a0@vishnu.edu.in>
2026-09-25 22:02:51 +0530

    Fix Render PostgreSQL connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,30 +2,31 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || process.env.DB_NAME,
-  process.env.DATABASE_URL
-    ? {}
-    : {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "postgres",
-      },
-  {
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false,
-
-    ...(process.env.DATABASE_URL && {
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
       },
-    }),
-  }
-);
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
