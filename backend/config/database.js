448105b6cef9fbc197ff50abf5da3efc5baf72a2
Prem Kumar <24pa1a05a0@vishnu.edu.in>
2026-09-25 22:00:34 +0530

    Fix production database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,30 @@
 const { Sequelize } = require("sequelize");
+
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL || process.env.DB_NAME,
+  process.env.DATABASE_URL
+    ? {}
+    : {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+      },
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
+
+    ...(process.env.DATABASE_URL && {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }),
   }
 );
 
